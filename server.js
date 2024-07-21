8f9de28106d67415e6d83ffffd4e5d39b6b9997a
alihadi202 <202008189@stu.uob.edu.bh>
2024-07-21 14:47:24 +0300

    fruits

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const morgan = require("morgan");
 
 require('./config/database'); //database code
 
+const Fruit = require("./models/fruit.js");
+
 const app = express();
 app.use(morgan ('dev'));
 
@@ -16,7 +18,13 @@ app.use(morgan ('dev'));
 // GET /
 app.get("/", async (req, res , next) => {
     res.render("index.ejs");
-  });
+});
+
+// GET /fruits/new
+app.get("/fruits/new", (req, res, next) => {
+    res.render("fruits/new.ejs");
+});
+
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
