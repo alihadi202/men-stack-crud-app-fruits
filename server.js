eb5da54b0312976d4c0e6f939c952daa0ce9183b
alihadi202 <202008189@stu.uob.edu.bh>
2024-07-21 14:11:39 +0300

    connection

---
+++ server.js
@@ -0,0 +1,23 @@
+// Here is where we import modules
+
+const dotenv = require("dotenv"); // require package
+dotenv.config(); // Loads the environment variables from .env file
+
+const express = require("express");// We begin by loading Express
+const morgan = require("morgan");
+
+require('./config/database'); //database code
+
+const app = express();
+app.use(morgan ('dev'));
+
+
+
+// GET /
+app.get("/", async (req, res , next) => {
+    res.render("index.ejs");
+  });
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
