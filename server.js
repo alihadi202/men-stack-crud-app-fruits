e233344ee68787ff18eb599177d9359b37f2bbb4
alihadi202 <202008189@stu.uob.edu.bh>
2024-07-21 16:55:08 +0300

    melon

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ dotenv.config(); // Loads the environment variables from .env file
 
 const express = require("express");// We begin by loading Express
 const morgan = require("morgan");
+const methodOverride = require("method-override");
 
 require('./config/database'); //database code
 
 const Fruit = require("./models/fruit.js");
 
 const app = express();
+app.use(methodOverride("_method"));
 app.use(morgan ('dev'));
 
 
@@ -48,6 +50,35 @@ app.get("/fruits", async (req, res, next) => {
 app.get("/fruits/:fruitId", async (req, res, next) => {
     const foundFruit = await Fruit.findById(req.params.fruitId);
     res.render("fruits/show.ejs", { fruit: foundFruit });
+});
+
+app.delete("/fruits/:fruitId", async (req, res, next) => {
+    await Fruit.findByIdAndDelete(req.params.fruitId);
+    res.redirect("/fruits");
+});
+
+// GET localhost:3000/fruits/:fruitId/edit
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    console.log(foundFruit);
+    res.render("fruits/edit.ejs", {
+        fruit: foundFruit,
+      });
+});
+
+app.put("/fruits/:fruitId", async (req, res) => {
+    // Handle the 'isReadyToEat' checkbox data
+    if (req.body.isReadyToEat === "on") {
+      req.body.isReadyToEat = true;
+    } else {
+      req.body.isReadyToEat = false;
+    }
+    
+    // Update the fruit in the database
+    await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+  
+    // Redirect to the fruit's show page to see the updates
+    res.redirect(`/fruits/${req.params.fruitId}`);
 });
 
 app.listen(3000, () => {
