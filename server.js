79e1d78d2ed1202dc05fb327a12966c86e735d83
alihadi202 <202008189@stu.uob.edu.bh>
2024-07-21 16:24:03 +0300

    nerf miner

--- server.js
+++ server.js
@@ -25,6 +25,30 @@ app.get("/fruits/new", (req, res, next) => {
     res.render("fruits/new.ejs");
 });
 
+app.use(express.urlencoded({ extended: false }));
+
+// POST /fruits
+app.post("/fruits", async (req, res) => {
+    if (req.body.isReadyToEat === "on") {
+        req.body.isReadyToEat = true;
+    } else {
+        req.body.isReadyToEat = false;
+    }
+    await Fruit.create(req.body);
+    res.redirect("/fruits/new");
+});
+
+// GET /fruits
+app.get("/fruits", async (req, res, next) => {
+    const allFruits = await Fruit.find();
+    console.log(allFruits);
+    res.render("fruits/index.ejs", { fruits: allFruits });
+});
+
+app.get("/fruits/:fruitId", async (req, res, next) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render("fruits/show.ejs", { fruit: foundFruit });
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
